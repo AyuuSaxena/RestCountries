5cc5314376c4449bc0bff23c19db6f2ffaf5afc5
Ayush saxena <42864535+ayush105@users.noreply.github.com>
2024-03-24 20:01:33 +0530

    updated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,10 +7,13 @@ import CountryNotfound from "./CountryNotfound.jsx";
 
 export default function Card() {
 
+
     let params = useParams(); // to get query params value from url
     let countryName = params.country;
+
     const [countryData, setcountryData] = useState(null);
     const [notFound, setnotFound] = useState(false)
+
 
     useEffect(() => {
 
@@ -36,27 +39,37 @@ export default function Card() {
                     region: object.region,
                     subRegion: object.subregion,
                     capital: object.capital,
-                    borderCountries: object.borders?.join() === undefined ? "None" : object.borders?.join(" , "),
+                    borderCountries: [], //?.join(" , ")
                     tld: object.tld?.join(" , "),
                     currency: Object.values(object.currencies)[0].name.concat(currencySymbol), // object.value converts object to array otherwise we can't access its properties because its key changes with country change/data 
                     language: Object.values(object.languages)?.join(" , ")
                 };
 
-                setcountryData(filteredData)
+                setcountryData(filteredData);
+
+                if(!object.borders){
+                    object.borders = [];
+                }
+
+                Promise.all(object.borders.map((bdr) => {
+                   return fetch(`https://restcountries.com/v3.1/alpha/${bdr}`)
+                        .then(res => res.json())
+                        .then(([data]) => data.name.common)
+                })).then(borders => setcountryData(previousState => ({ ...previousState, borderCountries: borders })))
+
+                
             } catch (error) {
                 setnotFound(true)
             }
         }
         countryDataFetch();
-    }, [])
-
-    // console.log(countryData)
+    }, [countryName])
 
 
-    return notFound ? <CountryNotfound/> : countryData === null ? (<Loading />) : ( // ternary operator for loading because it takes time to get the country data
+    return notFound ? <CountryNotfound /> : countryData === null ? (<Loading />) : ( // ternary operator for loading because it takes time to get the country data
 
         <div className="container">
-            <button className="btn back" onClick={()=> history.back()}><i className="fa-solid fa-arrow-left"></i> Back</button>
+            <button className="btn back" onClick={() => history.back()}><i className="fa-solid fa-arrow-left"></i> Back</button>
             <div className="cardContainer">
                 <div className="flag">
                     <img src={countryData.png} alt="" />
@@ -64,15 +77,40 @@ export default function Card() {
                 <div className="content">
                     <h1>{countryData.countryName}</h1>
                     <div className="details">
-                        <p><b>Native Name :</b>  {countryData.nativeName}</p>
-                        <p><b>Population :</b> {countryData.population}</p>
-                        <p><b>Region :</b> {countryData.region} </p>
-                        <p><b>Sub region :</b> {countryData.subRegion} </p>
-                        <p><b>Capital :</b> {countryData.capital} </p>
-                        <p><b>Border Countries :</b> {countryData.borderCountries}</p>
-                        <p><b>Top level domain :</b> {countryData.tld} </p>
-                        <p><b>Currencies :</b> {countryData.currency} </p>
-                        <p><b>Languages :</b> {countryData.language}</p>
+                        <p >
+                            <b>Native Name :</b>  {countryData.nativeName}
+                        </p>
+                        <p>
+                            <b>Population :</b> {countryData.population}
+                        </p>
+                        <p>
+                            <b>Region :</b> {countryData.region} 
+                        </p>
+                        <p>
+                            <b>Sub region :</b> {countryData.subRegion} 
+                        </p>
+                        <p>
+                            <b>Capital :</b> {countryData.capital} 
+                        </p>
+                        
+                        <p>
+                            <b>Top level domain :</b> {countryData.tld} 
+                        </p>
+                        <p>
+                            <b>Currencies :</b> {countryData.currency} 
+                        </p>
+                        <p>
+                            <b>Languages :</b> {countryData.language}
+                        </p>
+                        {countryData.borderCountries.length !== 0 && <p className="d-flex flex-wrap ">
+                            <b>Border Countries :</b> {
+                                
+                                countryData.borderCountries.map((border) => {
+                                    return <Link className="btn btn-light m-1 py-0 d-inline" key={border} to={`/country/${border}`} >{border}</Link>
+                                })
+                            }
+                        </p>}
+                        
                     </div>
 
                 </div></div>
